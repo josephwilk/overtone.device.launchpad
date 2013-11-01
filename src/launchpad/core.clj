757f3f69d69caace8497308fae0793307510a57d
Joseph Wilk <joe@josephwilk.net>
2013-11-01 18:57:31 +0100

    naming

--- src/launchpad/core.clj
+++ src/launchpad/core.clj
@@ -1,12 +1,12 @@
 (ns launchpad.core
   (:require [overtone.studio.midi :as midi]
-            [launchpad.core :as core]))
+            [launchpad.device :as device]))
 
 (defn boot! []
   (defonce launchpad-connected-receivers (midi/midi-find-connected-receivers "Launchpad"))
   (defonce launchpad-connected-devices   (midi/midi-find-connected-devices "Launchpad"))
-  (defonce launchpad-stateful-devices    (map core/stateful-launchpad launchpad-connected-devices))
-  (defonce launchpad-kons                (core/merge-launchpad-kons launchpad-connected-receivers launchpad-stateful-devices)))
+  (defonce launchpad-stateful-devices    (map device/stateful-launchpad launchpad-connected-devices))
+  (defonce launchpad-kons                (device/merge-launchpad-kons launchpad-connected-receivers launchpad-stateful-devices)))
 
 (comment
   (boot!))
