c59fc20527ac3c4c70111a947395a3bddda21074
Joseph Wilk <joe@josephwilk.net>
2013-11-02 18:47:56 +0100

    Just turn led off for old mode

--- src/launchpad/state_maps.clj
+++ src/launchpad/state_maps.clj
@@ -1,33 +1,29 @@
 (ns launchpad.state-maps
   (:require [launchpad.device :as device]))
 
-(def modes [:up :down :left :right])
-
-;;FIXME: This is wasting precious refresh events.
-;; only turn off whats on.
-(defn reset-direction-modes [launchpad]
-  (doseq [mode modes] (device/led-off* launchpad mode)))
+(defn turn-off-old-active-mode [launchpad]
+  (device/led-off* launchpad (:active @(:state launchpad))))
 
 (defn up-mode [launchpad]
-  (reset-direction-modes launchpad)
+  (turn-off-old-active-mode launchpad)
   (device/led-on* launchpad :up)
   (swap! (:state launchpad) assoc :active :up)
   (device/render-grid launchpad (:up @(:state launchpad))))
 
 (defn down-mode [launchpad]
-  (reset-direction-modes launchpad)
+  (turn-off-old-active-mode launchpad)
   (device/led-on* launchpad :down)
   (swap! (:state launchpad) assoc :active :down)
   (device/render-grid launchpad (:down @(:state launchpad))))
 
 (defn left-mode [launchpad]
-  (reset-direction-modes launchpad)
+  (turn-off-old-active-mode launchpad)
   (device/led-on* launchpad :left)
   (swap! (:state launchpad) assoc :active :left)
   (device/render-grid launchpad (:left @(:state launchpad))))
 
 (defn right-mode [launchpad]
-  (reset-direction-modes launchpad)
+  (turn-off-old-active-mode launchpad)
   (device/led-on* launchpad :right)
   (swap! (:state launchpad) assoc :active :right)
   (device/render-grid launchpad (:right @(:state launchpad))))
