4c4438d0542041d16a6f092740fba5b02c4f981c
josephwilk <joe@josephwilk.net>
2013-11-19 15:22:08 +0100

    test private fn

--- test/launchpad/t_device.clj
+++ test/launchpad/t_device.clj
@@ -1,14 +1,14 @@
 (ns launchpad.t-device
-  (:use midje.sweet
-        launchpad.device))
+  (:use midje.sweet)
+  (:require [launchpad.device :as device]))
 
 (fact "velocity maps color and intensity to a decimal signal"
-  (velocity {:color :red    :intensity 3}) => 15
-  (velocity {:color :yellow :intensity 3}) => 63
-  (velocity {:color :green  :intensity 3}) => 60)
+  (#'device/velocity {:color :red    :intensity 3}) => 15
+  (#'device/velocity {:color :yellow :intensity 3}) => 63
+  (#'device/velocity {:color :green  :intensity 3}) => 60)
 
 (fact "invalid colors return 0"
-  (velocity {:color :brown :intensity 0}) => 0)
+  (#'device/velocity {:color :brown :intensity 0}) => 0)
 
 (fact "intensity is capped at 3"
-  (velocity {:color :red :intensity 100000}) => 15)
+  (#'device/velocity {:color :red :intensity 100000}) => 15)
