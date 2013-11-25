c14a03040069c2eb1d819df98c25eb3bf8875084
Joseph Wilk <joe@josephwilk.net>
2013-11-25 21:56:54 +0100

    shiny new overtone

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.9.0-SNAPSHOT"]
+                 [overtone "0.9.1"]
                  [overtone/at-at "1.2.0"]]
 
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
